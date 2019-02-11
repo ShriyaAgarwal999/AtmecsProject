5546471ac4f6f37876ab6be017ba7012443a681c
Shriya Agarwal <shriya.agarwal@ATMECS.CORP>
2019-02-11 18:17:50 +0530

    first commit

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+recipes: Recipe[]=[new Recipe('Test recipe','This is the description','https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto,w_600,h_750,c_fit,fl_strip_profile/https://s3.amazonaws.com/pixtruder/original_images/17c6ec7292ce74c4f38c71ee4816925c46ffd4bf'),
+new Recipe('Test recipe','This is the description','https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto,w_600,h_750,c_fit,fl_strip_profile/https://s3.amazonaws.com/pixtruder/original_images/17c6ec7292ce74c4f38c71ee4816925c46ffd4bf')];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
